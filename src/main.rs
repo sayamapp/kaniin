a0ccb78cc2c17da230a8a3551b80e6b4fb7e47cc
sayamapp <sayamapp@gmail.com>
2021-03-27 20:03:41 +0900

    0.2 start

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,12 @@ use crate::gameover::GameOverPlugin;
 
 
 fn main() {
-    App::build()
-        .add_resource(WindowDescriptor {
+    let mut app = App::build();
+     
+        #[cfg(target_arch = "wasm32")]
+        app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+        app.add_resource(WindowDescriptor {
             width: WINDOW_WIDTH,
             height: WINDOW_HEIGHT,
             title: "kanirock".to_string(),
