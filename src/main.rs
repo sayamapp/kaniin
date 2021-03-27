3bae1d151b5bafdc024092f84614801eb1975295
sayamapp <sayamapp@gmail.com>
2021-03-27 20:57:11 +0900

    ugokanai

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,46 @@
 mod consts;
-mod title;
 mod score;
+mod title;
 
-use bevy::{input::system::exit_on_esc_system, prelude::*};
-use score::ScorePlugin;
 use crate::consts::*;
 use crate::title::TitlePlugin;
+use bevy::{input::system::exit_on_esc_system, prelude::*};
+use score::ScorePlugin;
 
 // 後で移動
 mod background;
-mod player;
-mod rock;
-mod ufo;
-mod rock_spawner;
 mod bullet;
 mod collision;
 mod gameover;
+mod player;
+mod rock;
+mod rock_spawner;
+mod ufo;
 use crate::background::BackgroundPlugin;
-use crate::player::PlayerPlugin;
-use crate::rock::RockPlugin;
-use crate::ufo::UfoPlugin;
-use crate::rock_spawner::RockSpawnerPlugin;
 use crate::bullet::BulletPlugin;
 use crate::collision::CollisionPlugin;
 use crate::gameover::GameOverPlugin;
-
+use crate::player::PlayerPlugin;
+use crate::rock::RockPlugin;
+use crate::rock_spawner::RockSpawnerPlugin;
+use crate::ufo::UfoPlugin;
 
 fn main() {
-    let mut app = App::build();
-     
-        #[cfg(target_arch = "wasm32")]
-        app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    // let mut app = App::build();
+    // app.add_resource(Msaa { samples: 4 });
+    // .add_resource(WindowDescriptor {;
+    //     width: WINDOW_WIDTH,
+    //     height: WINDOW_HEIGHT,
+    //     ..Default::default()
+    // });
 
-        app.add_resource(WindowDescriptor {
+    // #[cfg(target_arch = "wasm32")]
+    // app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    App::build()
+        .add_resource(WindowDescriptor {
             width: WINDOW_WIDTH,
             height: WINDOW_HEIGHT,
-            title: "kanirock".to_string(),
             ..Default::default()
         })
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
@@ -45,15 +50,12 @@ fn main() {
             APP_STATE_STAGE,
             StateStage::<AppState>::default(),
         )
-
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system()) // Cameras 
-        .add_system(exit_on_esc_system.system())  
-
+        .add_startup_system(setup.system()) // Cameras
+        .add_system(exit_on_esc_system.system())
         // add my plugins
         .add_plugin(TitlePlugin)
         .add_plugin(BackgroundPlugin)
-
         .add_plugin(PlayerPlugin)
         .add_plugin(RockPlugin)
         .add_plugin(UfoPlugin)
@@ -62,7 +64,6 @@ fn main() {
         .add_plugin(CollisionPlugin)
         .add_plugin(GameOverPlugin)
         .add_plugin(ScorePlugin)
-
         .run();
 }
 
@@ -77,10 +78,8 @@ fn setup(
 
     let rock_texture_handle = asset_server.load(ROCK_TEXTURE);
     let bullet_texture_handle = asset_server.load(TURBO_FISH_TEXTURE);
-    commands
-        .insert_resource(Materials {
-            rock_material: materials.add(rock_texture_handle.into()),
-            bullet_material: materials.add(bullet_texture_handle.into()),
-        });
+    commands.insert_resource(Materials {
+        rock_material: materials.add(rock_texture_handle.into()),
+        bullet_material: materials.add(bullet_texture_handle.into()),
+    });
 }
-
