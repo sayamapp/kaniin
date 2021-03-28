907b24fa788c8d016ce2f3614a50c5e753db5bf9
sayamapp <sayamapp@gmail.com>
2021-03-28 19:07:28 +0900

    ugoku けど一旦終わり。

--- src/main.rs
+++ src/main.rs
@@ -34,15 +34,8 @@ fn main() {
     //     ..Default::default()
     // });
 
-    // #[cfg(target_arch = "wasm32")]
-    // app.add_plugin(bevy_webgl2::WebGL2Plugin);
-
-    App::build()
-        .add_resource(WindowDescriptor {
-            width: WINDOW_WIDTH,
-            height: WINDOW_HEIGHT,
-            ..Default::default()
-        })
+     App::build()
+        // .add_resource(Msaa{samples: 4})
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_resource(State::new(AppState::Title))
         .add_stage_after(
@@ -50,21 +43,47 @@ fn main() {
             APP_STATE_STAGE,
             StateStage::<AppState>::default(),
         )
+        
+        // .add_resource(WindowDescriptor {
+        //     width: WINDOW_WIDTH,
+        //     height: WINDOW_HEIGHT,
+        //     ..Default::default()
+        // })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system()) // Cameras
         .add_system(exit_on_esc_system.system())
-        // add my plugins
-        .add_plugin(TitlePlugin)
-        .add_plugin(BackgroundPlugin)
-        .add_plugin(PlayerPlugin)
-        .add_plugin(RockPlugin)
-        .add_plugin(UfoPlugin)
-        .add_plugin(RockSpawnerPlugin)
-        .add_plugin(BulletPlugin)
-        .add_plugin(CollisionPlugin)
-        .add_plugin(GameOverPlugin)
-        .add_plugin(ScorePlugin)
-        .run();
+
+    // #[cfg(target_arch = "wasm32")]
+    // app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    // app.add_resource(WindowDescriptor {
+    //     width: WINDOW_WIDTH,
+    //     height: WINDOW_HEIGHT,
+    //     ..Default::default()
+    // })
+    // .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+    // .add_resource(State::new(AppState::Title))
+    // .add_stage_after(
+    //     stage::UPDATE,
+    //     APP_STATE_STAGE,
+    //     StateStage::<AppState>::default(),
+    // )
+    // .add_plugins(DefaultPlugins)
+    // .add_startup_system(setup.system()) // Cameras
+    // .add_system(exit_on_esc_system.system())
+    // add my plugins
+     
+    .add_plugin(TitlePlugin)
+    .add_plugin(BackgroundPlugin)
+    .add_plugin(PlayerPlugin)
+    .add_plugin(RockPlugin)
+    .add_plugin(UfoPlugin)
+    .add_plugin(RockSpawnerPlugin)
+    .add_plugin(BulletPlugin)
+    .add_plugin(CollisionPlugin)
+    .add_plugin(GameOverPlugin)
+    .add_plugin(ScorePlugin)
+    .run();
 }
 
 fn setup(
--- src/rock_spawner.rs
+++ src/rock_spawner.rs
@@ -11,7 +11,8 @@ use crate::rock::{Rock, RockSize};
 pub struct RockSpawnerPlugin;
 impl Plugin for RockSpawnerPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.on_state_update(APP_STATE_STAGE, AppState::Game, rock_spawner.system());
+        app.on_state_enter(APP_STATE_STAGE, AppState::Game, setup_debug.system())
+            .on_state_update(APP_STATE_STAGE, AppState::Game, rock_spawner.system());
     }
 }
 pub struct RockSpawnTimer(Timer);
@@ -19,6 +20,12 @@ impl Default for RockSpawnTimer {
     fn default() -> Self {
         Self(Timer::new(Duration::from_millis(SPAWN_DURATION), true))
     }
+}
+
+pub struct DebugRockCount(usize);
+
+pub fn setup_debug(commands: &mut Commands) {
+    commands.insert_resource(DebugRockCount(1));
 }
 
 pub fn rock_spawn(
@@ -29,9 +36,9 @@ pub fn rock_spawn(
     velocity_x: f32,
 ) {
     let scale = match size {
-        RockSize::Large => {8.0}
-        RockSize::Midium => {4.0}
-        RockSize::Small => {2.0}
+        RockSize::Large => 8.0,
+        RockSize::Midium => 4.0,
+        RockSize::Small => 2.0,
     };
 
     commands
@@ -57,30 +64,38 @@ fn rock_spawner(
     mut timer: Local<RockSpawnTimer>,
     query: Query<&Transform, With<UFO>>,
     materials: Res<Materials>,
+    mut count: ResMut<DebugRockCount>,
 ) {
-    if timer.0.tick(time.delta_seconds()).finished() {
+    // if timer.0.tick(time.delta_seconds()).finished() {
+    let c = count.0;
+    if time.seconds_since_startup() / 5.0 > c as f64 {
+        count.0 = c + 1;
         let mut pos = 0.0;
         for transform in query.iter() {
             pos = transform.translation.x;
         }
 
-        let random_size = rand::thread_rng().gen_range(1, 100);
-        let random_velocity_x = rand::thread_rng().gen_range(1.0, 4.0);
+        // let random_size = rand::thread_rng().gen_range(1, 100);
+        // let random_velocity_x = rand::thread_rng().gen_range(1.0, 4.0);
+        let random_size = 10;
+        let random_velocity_x = 2.0;
 
-        if random_size < 50 {
+        if c % 2 != 0 {
             rock_spawn(
-                commands, 
-                &materials, 
-                RockSize::Midium, 
-                Vec3::new(pos, ROCK_SPAWN_POSITION_Y, 0.0), 
-                random_velocity_x);
+                commands,
+                &materials,
+                RockSize::Midium,
+                Vec3::new(pos, ROCK_SPAWN_POSITION_Y, 0.0),
+                random_velocity_x,
+            );
         } else {
             rock_spawn(
-                commands, 
-                &materials, 
-                RockSize::Large, 
-                Vec3::new(pos, ROCK_SPAWN_POSITION_Y, 0.0), 
-                random_velocity_x);
+                commands,
+                &materials,
+                RockSize::Large,
+                Vec3::new(pos, ROCK_SPAWN_POSITION_Y, 0.0),
+                random_velocity_x,
+            );
         }
     }
 }
